c8ae34007fcd8a9d2c9d030fe22c7bbd98551bc2
INno <inno.fxy@gmail.com>
2025-07-23 19:39:47 +0800

    update

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -1,8 +1,17 @@
+# python -m apps.demo \
+# -r "Dumbledore" \
+# -n demo_HP_1_4_32b_512_long \
+# -u http://0.0.0.0:8000/v1 \
+# -m Qwen3-32B \
+# -l en \
+# -g \
+# --chat \
+# --short 
 python -m apps.demo \
 -r "Dumbledore" \
 -n demo_HP_1_4_32b_512_long \
--u http://0.0.0.0:8000/v1 \
--m Qwen3-32B \
+-u http://210.28.135.96:8000/v1 \
+-m Qwen2.5-14B-Instruct \
 -l en \
 -g \
 --chat \

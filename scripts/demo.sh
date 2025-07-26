f53ab5a3dbe5fd234c856c7d395409d62042f770
ZhanxyR <zhanxy@smail.nju.edu.cn>
2025-07-26 13:43:50 +0800

    Update demo.sh

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -1,6 +1,6 @@
 python -m apps.demo \
 --roles "Dumbledore" \
---name demo_HP_1_4_32b_512_long \
+--name demo_Harry_Potter_1_4_Qwen25_32b_512_long \
 --url http://0.0.0.0:8000/v1 \
 --model Qwen3-32B \
 --language en \

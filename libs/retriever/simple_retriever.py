2635313215f6f4a5ff12c1f0b675cf0de9525d08
ZhanxyR <zhanxy@smail.nju.edu.cn>
2025-08-02 22:56:17 +0800

    Clean cache, reduce memory usage.

--- libs/retriever/simple_retriever.py
+++ libs/retriever/simple_retriever.py
@@ -84,8 +84,16 @@ class Retriever:
         
         pairs = [[query, doc.page_content] for doc in docs]
 
+        del pairs
+        torch.cuda.empty_cache()
+
         scores = self.get_scores(pairs)
         
         ranked_docs = sorted(zip(docs, scores), key=lambda x: x[1], reverse=True)
+
+        results = [doc for doc, _ in ranked_docs[:top_k]]
+
+        del scores, ranked_docs
+        torch.cuda.empty_cache()
         
-        return [doc for doc, _ in ranked_docs[:top_k]]
+        return results
